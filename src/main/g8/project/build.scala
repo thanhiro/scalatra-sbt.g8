d9d861a6ef8224281adaaafde5c8c100576be0a1
Timo Hanhirova <timo.hanhirova@fudeco.com>
2013-05-17 08:51:20 +0300

    Refinement

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -10,6 +10,14 @@ object $name;format="Camel"$Build extends Build {
   val ScalaVersion = "$scala_version$"
   val ScalatraVersion = "$scalatra_version$"
 
+  val deps = Seq(
+    "org.scalatra" %% "scalatra" % ScalatraVersion,        
+    "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+    "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
+    "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
+    "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+  )
+
   lazy val project = Project (
     "$name;format="norm"$",
     file("."),
@@ -19,13 +27,7 @@ object $name;format="Camel"$Build extends Build {
       version := Version,
       scalaVersion := ScalaVersion,
       resolvers += Classpaths.typesafeReleases,
-      libraryDependencies ++= Seq(
-        "org.scalatra" %% "scalatra" % ScalatraVersion,        
-        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-        "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
-        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
-      )
+      libraryDependencies ++= deps
     )
   )
 }
